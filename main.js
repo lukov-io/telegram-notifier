84a330e42ca102186ab4b0f8183100ccfadc6946
razz1k <zdeadmenz@gmail.com>
2024-11-12 19:42:39 +0300

    action.yml added

--- main.js
+++ main.js
@@ -1,40 +1,62 @@
-const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN,
-  TG_CHAT_ID = process.env.TG_CHAT_ID,
-  GITHUB_RUNNER_TOKEN = process.env.GITHUB_RUNNER_TOKEN,
-  GITHUB_REPO = process.env.GITHUB_REPO,
-  GITHUB_RUN_ID = process.env.GITHUB_RUN_ID,
+const TG_BOT_TOKEN = process.env.INPUT_TG_BOT_TOKEN,
+  TG_CHAT_ID = process.env.INPUT_TG_CHAT_ID,
+  TG_TOPIC_ID = process.env.INPUT_TG_TOPIC_ID,
+  GITHUB_RUNNER_TOKEN = process.env.INPUT_GITHUB_RUNNER_TOKEN,
+  GITHUB_REPO = process.env.INPUT_GITHUB_REPO,
+  GITHUB_RUN_ID = process.env.INPUT_GITHUB_RUN_ID,
+  NEEDS = process.env.INPUT_NEEDS,
   GITHUB_FETCH_OPTIONS = {
     headers: {
       'Accept': 'vnd.github+json',
       'Authorization': `Bearer ${GITHUB_RUNNER_TOKEN}`
     }
   };
+
 let jobs_urls = '',
+  msg_text = '',
+  json_needs,
   json_run,
-  json_jobs;
+  json_jobs,
+  conclusion;
 
 (async () => {
-  json_run = await fetch(
+  json_run = await getData(
     `https://api.github.com/repos/${GITHUB_REPO}/actions/runs/${GITHUB_RUN_ID}`,
-    GITHUB_FETCH_OPTIONS).then(data => {
-    return data.json()
-  });
-  json_jobs = await fetch(
+    GITHUB_FETCH_OPTIONS);
+  json_jobs = await getData(
     `https://api.github.com/repos/${GITHUB_REPO}/actions/runs/${GITHUB_RUN_ID}/jobs`,
-    GITHUB_FETCH_OPTIONS).then(data => {
-    return data.json()
-  });
+    GITHUB_FETCH_OPTIONS);
 
-  json_jobs.jobs.forEach(job => {
-    if (job.name !== 'notify') {
-      jobs_urls += `<a href="${job.html_url}">${job.name} - ${job.conclusion}</a>\n`;
+  if (json_run && json_jobs) {
+    conclusion = json_run.conclusion;
+
+    if (NEEDS) {
+      json_needs = JSON.parse(NEEDS);
+      let jobsArray = [];
+
+      Object.keys(json_needs).forEach(key => {
+        jobsArray.push(key);
+      });
+
+      json_jobs.jobs.forEach(job => {
+        if (jobsArray.includes(job.name)) {
+          jobs_urls += `<a href="${job.html_url}">${job.name} - ${job.conclusion}</a>\n`;
+        }
+
+        setConclusion(job.conclusion);
+      });
+    } else {
+      json_jobs.jobs.forEach(job => {
+        jobs_urls += `<a href="${job.html_url}">${job.name} - ${job.conclusion}</a>\n`;
+
+        setConclusion(job.conclusion);
+      });
     }
-  });
 
-  let msg_text = `
-<a href="${json_run.actor.html_url}">${json_run.actor.login}</a>'s workflow completed with <a href="${json_run.html_url}">status ${json_run.conclusion}</a>
-
-jobs info:\n${jobs_urls}`;
+    msg_text = `<a href="${json_run.actor.html_url}">${json_run.actor.login}</a>'s workflow completed with <a href="${json_run.html_url}">status ${conclusion}</a>, run attempt ${json_run.run_attempt}\n\njobs info:\n${jobs_urls}`;
+  } else {
+    msg_text = `tg notifier error on https://github.com/${GITHUB_REPO}/actions/runs/${GITHUB_RUN_ID}`;
+  }
 
   let fetch_body = {
     chat_id: TG_CHAT_ID,
@@ -43,7 +65,7 @@ jobs info:\n${jobs_urls}`;
     disable_notification: false
   };
 
-  console.log(fetch_body);
+  if (TG_TOPIC_ID) fetch_body.message_thread_id = TG_TOPIC_ID;
 
   await fetch(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
     {
@@ -53,5 +75,29 @@ jobs info:\n${jobs_urls}`;
       },
       body: `${JSON.stringify(fetch_body)}`
     })
-    .then((response) => console.log(response));
+    .catch((err) => console.log(err));
 })();
+
+function setConclusion(jobConclusion) {
+  if (json_run.conclusion === null) {
+    if (conclusion !== 'cancelled') {
+      if (conclusion !== 'failure') {
+        if (jobConclusion !== null) {
+          conclusion = jobConclusion;
+        }
+      }
+    }
+  }
+}
+async function getData(url, opt) {
+  try {
+    const response = await fetch(url, opt);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
