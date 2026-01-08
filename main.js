bbc6efa16db39e4f0e203323b5f3b1a802faf805
razz1k <zdeadmenz@gmail.com>
2026-01-08 15:35:07 +0300

    workflow name at the top of msg

--- main.js
+++ main.js
@@ -92,9 +92,9 @@ let jobs_urls = '',
         });
       }
 
-      msg_text = `${json_run.name} <br> Деплой на ветке <b><u>${GITHUB_REF_NAME}</u></b> завершен со статусом <a href="${json_run.html_url}">${conclusion}</a>, пользователем <a href="${json_run.actor.html_url}">${json_run.actor.login}</a>, попыток: ${json_run.run_attempt}\n\nПодзадачи:\n${jobs_urls}`;
+      msg_text = `${json_run.name} \nДеплой на ветке <b><u>${GITHUB_REF_NAME}</u></b> завершен со статусом <a href="${json_run.html_url}">${conclusion}</a>, пользователем <a href="${json_run.actor.html_url}">${json_run.actor.login}</a>, попыток: ${json_run.run_attempt}\n\nПодзадачи:\n${jobs_urls}`;
     } else {
-      msg_text = `${json_run.name} <br> Ошибка <a href="https://github.com/${GITHUB_REPO}/actions/runs/${GITHUB_RUN_ID}">деплоя</a>`;
+      msg_text = `${json_run.name} \nОшибка <a href="https://github.com/${GITHUB_REPO}/actions/runs/${GITHUB_RUN_ID}">деплоя</a>`;
     }
   }
 })();
